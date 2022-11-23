b8599b1bf53edc259ad7a39733dad2c1ea1ceaa4
Milan Lund <hello@milanlund.com>
2022-11-23 15:02:50 +0100

    Design updates

---
+++ static/gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({
+  browsers: ['last 2 versions']
+});
+const cleanCSS = require('gulp-clean-css');
+
+const cssTasks = [{
+  name: 'css-app',
+  config: {
+    src: [
+      'assets/css/src/general/fonts.less',
+      'assets/css/src/general/kentico-icons.less',
+      'assets/css/src/general/utilities.less',
+      'assets/css/src/general/data-toggle.less',
+      'assets/css/src/components/navigation.less',
+      'assets/css/src/components/breadcrumbs.less',
+      'assets/css/src/components/menu-content.less',
+      'assets/css/src/components/api-info.less',
+      'assets/css/src/components/code-block.less',
+      'assets/css/src/components/samples-block.less',
+      'assets/css/src/components/table.less',
+      'assets/css/src/components/callout.less',
+      'assets/css/src/components/footer.less',
+      'assets/css/src/components/autocomplete.less',
+      'assets/css/src/components/suggestion.less',
+      'assets/css/src/components/endpoint.less',
+      'assets/css/src/components/redocly-overrides.less',
+    ],
+    fileName: 'app.less'
+  }
+}];
+
+const processCss = (config) => {
+  return gulp.src(config.src)
+  .pipe(concat(config.fileName))
+  .pipe(less({
+    plugins: [autoprefix]
+  }))
+  .pipe(cleanCSS())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('assets/css'));
+};
+
+cssTasks.forEach((item) => {
+  gulp.task(item.name, () => processCss(item.config));
+});
+
+gulp.task('build-css', gulp.parallel(['css-app']));
+
+gulp.task('watch', (done) => {
+  gulp.watch('assets/css/src/**/*.less', gulp.series(['build-css']));
+  done();
+});
+
+gulp.task('default', gulp.series([...cssTasks.map((item) => item.name), 'watch']));
--- theme.ts
+++ theme.ts
@@ -1,11 +1,11 @@
 import { lighten, darken, readableColor } from 'polished';
 
 export const theme = {
-  // spacing: {
-  //   unit: 5,
+   spacing: {
+    unit: 4,
   //   sectionHorizontal: ({ spacing }) => spacing.unit * 8,
-  //   sectionVertical: ({ spacing }) => spacing.unit * 8,
-  // },
+    sectionVertical: 32,
+   },
   // breakpoints: {
   //   xs: 0,
   //   small: '550px',
@@ -17,69 +17,82 @@ export const theme = {
     primary: {
       main: '#231F20',
       // light: ({ colors }) => lighten(colors.tonalOffset, colors.primary.main),
-      // dark: ({ colors }) => darken(colors.tonalOffset, colors.primary.main),
+      // dark: 'yellow', ({ colors }) => darken(colors.tonalOffset, colors.primary.main),
       // contrastText: ({ colors }) => readableColor(colors.primary.main),
     },
+    secondary: {
+      contrastText: "#231F20",
+      light: "#f3f3f3",
+      main: "transparent",
+    },
     success: {
-       main: '#44B77A',
+       main: '#36A284',
     //   light: ({ colors }) => lighten(colors.tonalOffset * 3, colors.success.main),
     //   dark: ({ colors }) => darken(colors.tonalOffset, colors.success.main),
     //   contrastText: ({ colors }) => readableColor(colors.success.main),
     },
-    // error: {
-    //   main: '#e53935',
+    error: {
+       main: '#B72929',
     //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.error.main),
     //   dark: ({ colors }) => darken(colors.tonalOffset, colors.error.main),
     //   contrastText: ({ colors }) => readableColor(colors.error.main),
-    // },
-    // warning: {
-    //   main: '#d4ad03',
+    },
+    warning: {
+      main: '#d4ad03',
     //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.warning.main),
     //   dark: ({ colors }) => darken(colors.tonalOffset, colors.warning.main),
     //   contrastText: ({ colors }) => readableColor(colors.warning.main),
-    // },
-    // info: {
-    //   main: '#4782cb',
+    },
+    info: {
+      main: '#f5e1da',
     //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.info.main),
     //   dark: ({ colors }) => darken(colors.tonalOffset, colors.info.main),
     //   contrastText: ({ colors }) => readableColor(colors.info.main),
-    // },
-    text: {
-      primary: '#424242',
-      // secondary: '#4e566d',
     },
-    // border: {
-    //   dark: 'rgba(0,0,0, 0.15)',
-    //   light: '#ffffff',
-    // },
-    // responses: {
-    //   success: {
-    //     color: ({ colors }) => colors.success.main,
-    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.success.main),
-    //   },
-    //   error: {
-    //     color: ({ colors }) => colors.error.main,
-    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.error.main),
-    //   },
-    //   redirect: {
-    //     color: ({ colors }) => colors.warning.main,
-    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.responses.redirect.color),
-    //   },
-    //   info: {
-    //     color: ({ colors }) => colors.info.main,
-    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.responses.info.color),
-    //   },
-    // },
+    text: {
+      light: "#231F20",
+      primary: "#231F20",
+      secondary: "#231F20",
+    },
+    border: {
+      dark: 'transparent',
+      light: '#e8e8e8',
+    },
+    accent: {
+      dark: "#3195a6"
+    },
+    responses: {
+      success: {
+        //     color: ({ colors }) => colors.success.main,
+        backgroundColor: '#ecf6f2',
+        borderColor: '#44B77A'
+      },
+      error: {
+        //     color: ({ colors }) => colors.error.main,
+        backgroundColor: '#f7e9e9',
+        borderColor: '#B72929'
+      },
+      redirect: {
+        //     color: ({ colors }) => colors.warning.main,
+        backgroundColor: '#fff9ee',
+        borderColor: '#FBC15E'
+      },
+      info: {
+        //     color: ({ colors }) => colors.info.main,
+        backgroundColor: '#f5e1da',
+        borderColor: '#F05A22'
+      },
+    },
     http: {
-      get: '#6bbd5b',
-      post: '#248fb2',
-      put: '#9b708b',
-      options: '#d3ca12',
-      patch: '#e09d43',
-      delete: '#e27a7a',
-      basic: '#999',
-      link: '#31bbb6',
-      head: '#c167e4',
+      get: '#44B77A',
+      post: '#8054B4',
+      put: '#1C60BF',
+      options: '#EBB500',
+      patch: '#F05A22',
+      delete: '#C42D61',
+      basic: '#96A236',
+      link: '#908E8F',
+      head: '#231F20',
     },
     // navbar: {
     //   main: ({ colors }) => colors.primary.main,
@@ -93,8 +106,13 @@ export const theme = {
   },
 
   sidebar: {
-    backgroundColor: '#fafafa',
-    width: '260px',
+    backgroundColor: 'transparent',
+    width: '285px',
+    activeBgColor: 'transparent',
+    textColor: '#908E8F',
+    activeTextColor: '#231F20',
+    caretColor: '#3dcca8',
+    rightLineColor: 'transparent',
   },
   // tocPanel: {
   //   width: '240px',
@@ -103,38 +121,38 @@ export const theme = {
   typography: {
     fontSize: '16px',
     lineHeight: '1.5em',
-    fontWeightRegular: '400',
-    fontWeightBold: '600',
+    fontWeightRegular: '500',
+    fontWeightBold: '700',
     fontWeightLight: '300',
-    fontFamily: '"Source Sans Pro", sans-serif',
+    fontFamily: 'Work Sans, sans-serif',
     headings: {
-      fontFamily: '"Source Sans Pro", sans-serif',
-      fontWeight: '600',
+      fontFamily: 'Work Sans, sans-serif',
+      fontWeight: '700',
     },
-    // heading1: {
-    //   fontSize: '1.85714em',
+    heading1: {
+      fontSize: '2em',
     //   fontWeight: '600',
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
-    //   color: ({ colors }) => colors.primary.main,
+      color: '#231F20',
     //   capitalize: true,
-    // },
-    // heading2: {
-    //   fontSize: '1.57143em',
+     },
+    heading2: {
+        fontSize: '1.375em',
     //   fontWeight: '600',
     //   color: ({ colors }) => colors.text.primary,
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
     //   capitalize: false,
-    // },
-    // heading3: {
-    //   fontSize: '1.27em',
+    },
+    heading3: {
+       fontSize: '1.125em',
     //   fontWeight: '600',
     //   color: ({ colors }) => colors.text.primary,
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
     //   capitalize: false,
-    // },
+    },
     // heading4: {
     // // ...
     // },
@@ -145,40 +163,68 @@ export const theme = {
     // // ...
     // },
     code: {
-      fontSize: '13px',
-      fontFamily: '"Source Code Pro", sans-serif',
+      fontSize: '14px',
+      fontFamily: 'Inconsolata, Consolas, monospace',
       // fontWeight: ({ typography }) => typography.fontWeightRegular,
-      color: '#e53935',
-      backgroundColor: 'rgba(38, 50, 56, 0.04)',
+      color: '#393a34',
+      backgroundColor: '#f3f3f3',
       wrap: false,
     },
     links: {
-      color: ({ colors }) => colors.primary.main,
-      visited: ({ typography }) => typography.links.color,
-      hover: ({ typography }) => lighten(0.2, typography.links.color),
+      color: '#6552c4',
+      textDecoration: 'underline',
+      // visited: ({ typography }) => typography.links.color,
+      hover: ({ typography }) => '#6552c4',
     },
   },
   rightPanel: {
-    backgroundColor: '#263238',
+    backgroundColor: 'transparent',
     width: '40%',
-    // textColor: '#ffffff',
+    textColor: '#231F20',
   },
   schema: {
-    nestedBackground: '#fafafa',
-    // linesColor: theme => lighten( theme.colors.tonalOffset, desaturate(theme.colors.tonalOffset, theme.colors.primary.main) ),
-    // defaultDetailsWidth: '75%',
-    // typeNameColor: theme => theme.colors.text.secondary,
-    // typeTitleColor: theme => theme.schema.typeNameColor,
-    // requireLabelColor: theme => theme.colors.error.main,
-    // labelsTextSize: '0.9em',
-    // nestingSpacing: '1em',
-    // arrow: {
-    //   size: '1.1em',
-    //   color: theme => theme.colors.text.secondary,
-    // },
+    labelsTextSize: '0.875em',
+    linesColor: '#bcbbbb',
+    nestedBackground: '#f3f3f3',
+    requireLabelColor: '#C42D61',
+    typeNameColor: '#908E8F',
+    typeTitleColor: '#F05A22',
   },
-  // codeBlock: {
-  //   backgroundColor: ({ rightPanel }) => darken(0.1, rightPanel.backgroundColor),
-  //   tokens: {},
-  // },
+   codeBlock: {
+    borderRadius: '36px 36px 12px 36px',
+    backgroundColor: '#fff'
+   },
+   components: {
+    buttons: {
+      borderRadius: "26px",
+      hoverStyle: "background-color:#231F20;color:#fff",
+      fontWeight: "700",
+      boxShadow: "none",
+      hoverBoxShadow: "none",
+    },
+    httpBadges: {
+      color: "#fff",
+      fontFamily: "Work Sans, sans-serif",
+      fontWeight: "400",
+      sizes: {
+        small: {
+          fontSize: "10px",
+          lineHeight: "1.75em",
+        },
+        medium: {
+          fontSize: "14px",
+          lineHeight: "1.75em"
+        }
+      }
+    },
+    panels: {
+      backgroundColor: "transprent",
+      borderRadius: "24px 24px 8px 24px",
+    }
+  },
+  overrides: {
+    DownloadButton: {
+      custom: "padding: 12px 32px;font-size: 16px;box-shadow: 0 0 0 2px #231f20;"
+    }
+  }
 };
